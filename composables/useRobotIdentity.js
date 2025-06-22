749f51ead82e0a1a73ad9357a0cacf27c4c41097
Learntheropes <giovanni@learntheropes.xyz>
2025-06-22 19:39:30 -0300

    update to koala instructions

--- composables/useRobotIdentity.js
+++ composables/useRobotIdentity.js
@@ -1,21 +1,28 @@
 import * as openpgp from 'openpgp'
-import { createHash, randomBytes } from 'crypto'
+import { createHash } from 'crypto'
 import baseX from 'base-x'
 
-const base91 = baseX("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;=?@[]^_`{|}~"
-)
+const base91 = baseX("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;=?@[]^_`{|}~")
+
+function genRobotToken(length = 40) {
+  const array = new Uint8Array(length * 2)
+  crypto.getRandomValues(array)
+
+  const base64 = btoa(String.fromCharCode(...array))
+  const token = base64.replace(/[+/]/g, '').substring(0, length)
+
+  return token
+}
+
+function escapeHeaderContent(str) {
+  return str.replace(/\r?\n/g, '\\n').replace(/[\x00-\x1F\x7F]/g, '')
+}
 
 export const useRobotIdentity = async () => {
-  const rawToken = randomBytes(32)
-  const token = rawToken.toString('hex')
-  const tokenHash = createHash('sha256').update(rawToken).digest()
-  const tokenBase91 = base91.encode(tokenHash)
-  const nostrPubKey = createHash('sha256').update(rawToken).digest('hex')
-
-  console.log('tokenBase91', tokenBase91, 'length', tokenBase91.length)
-  console.log('tokenHash.length', tokenHash.length) // MUST be 32
-  console.log('tokenHash instanceof Buffer', tokenHash instanceof Buffer) // true
-  console.log('tokenBase91.length', tokenBase91.length) // 39 or 40
+  const token = genRobotToken(40)
+  const tokenHash = createHash('sha256').update(token).digest() // hash the token string
+  const tokenBase91 = base91.encode(tokenHash) // final token used in Authorization header
+  const nostrPubKey = createHash('sha256').update(token).digest('hex')
 
   const { privateKey, publicKey } = await openpgp.generateKey({
     type: 'rsa',
@@ -24,10 +31,6 @@ export const useRobotIdentity = async () => {
     passphrase: token,
     format: 'armored'
   })
-
-  function escapeHeaderContent(str) {
-  return str.replace(/\r?\n/g, '\\n').replace(/[\x00-\x1F\x7F]/g, '')
-}
 
   const armoredPublic = escapeHeaderContent(publicKey)
   const armoredPrivate = escapeHeaderContent(privateKey)
@@ -38,8 +41,6 @@ export const useRobotIdentity = async () => {
     `^| Private ${armoredPrivate}`,
     `^| Nostr ${nostrPubKey}`
   ].join(' ')
-
-  console.log('authorization:', authorization)
 
   return {
     token,
